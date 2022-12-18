3530409f10e33935bf5f7e3d71ead0b35a92fc14
Dhanajit Kapali <dhanajit.k@peopleinteractive.in>
2022-12-19 01:12:27 +0530

    Initial Commit

---
+++ BezierPathVisualizer/BezierPathVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BezierPathVisualizerApp.swift
+//  BezierPathVisualizer
+//
+//  Created by Dhanajit Kapali on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BezierPathVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
