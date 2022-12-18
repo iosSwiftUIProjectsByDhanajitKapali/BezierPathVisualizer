a8b63203e94caf6aa63f5aa90f4f1995f311f855
Dhanajit Kapali <dhanajit.k@peopleinteractive.in>
2022-12-19 01:27:39 +0530

    Added BezierPathVisualizer

--- BezierPathVisualizer/ContentView.swift
+++ BezierPathVisualizer/ContentView.swift
@@ -6,21 +6,79 @@
 //
 
 import SwiftUI
+import PureSwiftUI
+
+private let heartLayoutConfig = LayoutGuideConfig.grid(columns: 8, rows: 10)
 
 struct ContentView: View {
+    var screenWidth  = UIScreen.main.bounds.width
+    var screenHeight = UIScreen.main.bounds.height
+    @State var shouldShowControlPoints = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .center) {
+            Button {
+                shouldShowControlPoints.toggle()
+            } label: {
+                Toggle("", isOn: $shouldShowControlPoints)
+                    .frame(width: 100)
+            }
+            ZStack {
+                MyCustomShape(showControlPoints: $shouldShowControlPoints)
+                    .stroke(Color.black, lineWidth: 2)
+                    .layoutGuide(heartLayoutConfig)
+                    .frame(width: screenWidth/1.2, height: screenWidth/1.2)
+            }
         }
-        .padding()
     }
 }
 
+struct MyCustomShape: Shape {
+    @Binding var showControlPoints : Bool
+    func path(in rect: CGRect) -> Path {
+        let width = rect.width
+        let height = rect.height
+        return Path { path in
+            path.move(to: CGPoint(
+                x: rect.minX,
+                y: rect.minY+(height*0.1))
+            )
+            
+            path.curve(
+                CGPoint(
+                    x: rect.midX,
+                    y: rect.minY+(height*0.30)
+                ),
+                cp1: CGPoint(
+                    x: rect.minX + (width*0.3),
+                    y: rect.minY + (height*0.1)
+                ),
+                cp2: CGPoint(
+                    x: rect.minX + (width*0.2),
+                    y: rect.minY + (height*0.30)
+                ),
+                showControlPoints: showControlPoints
+            )
+            path.curve(
+                CGPoint(
+                    x: rect.maxX,
+                    y: rect.minY+(height*0.1)
+                ),
+                cp1: CGPoint(
+                    x: rect.maxX - (width*0.2),
+                    y: rect.minY+(height*0.30)
+                ),
+                cp2: CGPoint(
+                    x: rect.maxX - (width*0.3),
+                    y: rect.minY + (height*0.1)
+                ),
+                showControlPoints: showControlPoints
+            )
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .showLayoutGuides(true)
     }
 }
